19313331fe926f778fd3b6624ec7d51e5f0dd56a
Tom Tak <y0ung-gil@hanmail.net>
2021-06-20 23:17:30 +0900

    NodeJS 119~124

--- 04.task-manager/src/index.js
+++ 04.task-manager/src/index.js
@@ -6,6 +6,24 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("File must be a Word document"));
+    }
+    cb(undefined, true);
+  },
+});
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -13,18 +31,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById("60cd7826f785530aca00d139");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById("60cd771bdb13a80a8b5082d0");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-
-// main();
--- 04.task-manager/src/routers/task.js
+++ 04.task-manager/src/routers/task.js
@@ -16,9 +16,33 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?limit=10&skip=2
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   try {
-    await req.user.populate("tasks").execPopulate();
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+      match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
